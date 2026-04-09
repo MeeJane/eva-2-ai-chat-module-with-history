14ccaa97e02b69cd81542404593657fbd20a4e47
MeeJane <meejane2024@gmail.com>
2026-04-10 03:47:54 +0530

    Remove dynamic BASE URL configuration

--- chatbox_frontend/src/services/api.js
+++ chatbox_frontend/src/services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-// 🔥 Dynamic BASE URL (local + production)
 const BASE_URL =
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:8005/api/"
         : "https://jafawais0330.pythonanywhere.com/api/";
 
-// 🔥 Axios instance
 const api = axios.create({
     baseURL: BASE_URL,
 });

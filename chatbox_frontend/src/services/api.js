524eb63a57eb21b2995cbe16a9c078e2a14f6c59
erjas <meejane2024@gmail.com>
2026-04-10 03:37:17 +0530

    Fixed API config for deployment

--- chatbox_frontend/src/services/api.js
+++ chatbox_frontend/src/services/api.js
@@ -1,30 +1,14 @@
 import axios from "axios";
 
-function getCSRFToken() {
-    return document.cookie
-        .split("; ")
-        .find(row => row.startsWith("csrftoken="))
-        ?.split("=")[1];
-}
-
-// 🔥 Dynamic URL
+// 🔥 Dynamic BASE URL (local + production)
 const BASE_URL =
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:8005/api/"
         : "https://jafawais0330.pythonanywhere.com/api/";
 
+// 🔥 Axios instance
 const api = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true,
-});
-
-// 🔥 CSRF interceptor
-api.interceptors.request.use((config) => {
-    const csrfToken = getCSRFToken();
-    if (csrfToken) {
-        config.headers["X-CSRFToken"] = csrfToken;
-    }
-    return config;
 });
 
 export default api;
